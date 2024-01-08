949f34ad5ab2e18114f2dcb841caf728802b9ea3
7MMA7 <120013670+7MMA7@users.noreply.github.com>
2024-01-08 20:20:37 +0100

    lien briques et quadtree, problème dans l'insertion, quand on insert trop d'éléments (plus de 4-5 je crois), stack overflow mais je n'arrive pas à voir comment régler ça pour l'instant

--- lib/quadtree.mli
+++ lib/quadtree.mli
@@ -4,7 +4,7 @@
    décompose la surface de jeux en 4 partie égales puis réccurssivement ces 4 parties etc...
    jusqu'à arriver à des zones élémentaires de taille T : int*int pixels*)
 
-type vector2 = int*int (*seras implémenté plus en détail plus tard *)
+type vector2 = float*float (*seras implémenté plus en détail plus tard *)
 type 'a feuille = {position : vector2; value : 'a}
 
     (*un arbre est soit vide, soit une feuille, soit un noeud*)
@@ -14,13 +14,13 @@ type 'a tree =
         | Empty
         | Leaf of 'a feuille
         | Node of 'a qtree * 'a qtree * 'a qtree * 'a qtree
-and 'a qtree = {tree : 'a tree; size : int*int; resol : int*int}
+and 'a qtree = {tree : 'a tree; size : float*float; resol : float*float}
 
   (*l'arbre est-il vide ?*)
 val empty : 'a qtree -> bool
 
   (*création de l'arbre à partir de la taille de la fenêtre*)
-val create : int -> int -> int*int -> 'a qtree
+val create : float -> float -> float*float -> 'a qtree
 (*permet d'insérer un objet 'a à une position donnée par vector2*)
 val insert : 'a qtree -> 'a feuille-> 'a qtree
 val remove : 'a qtree -> vector2 -> 'a qtree
@@ -30,6 +30,6 @@ val isOccupied : 'a qtree -> vector2 -> 'a feuille option
      ça renvoie le nouveau vecteur vitesse de l'objet
      si l'objet ne collisionne pas, le vecteur vitesse est inchangé*)
   (*de plus, on renvoie le qtree à jour*)
-val colide : 'a qtree -> float*float -> float*float -> int -> 'a qtree * (float*float)* 'a feuille option 
+val colide : 'a qtree -> float*float -> float*float -> float -> 'a qtree * (float*float)* 'a feuille option 
 
 
