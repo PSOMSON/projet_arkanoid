9dae67139f6e15b3e723eb3a01f16e22a8d7326f
SliceOfArdath <nyan.pandora.loves@gmail.com>
2024-01-24 18:44:17 +0100

    BOUNCE DEEZ NUTS
    I WILL NOT APOLOGIZE FOR THE ACCIDENT OF MY BIRTH

--- lib/motion.ml
+++ lib/motion.ml
@@ -34,10 +34,25 @@ let integre dt acc flux =
 (*TODO Mise a jour d'etat!! *)
 module Motion (E : Env) =
 struct
+  let contact_x x dx = let (bx, tx, _) = E.bords
+  in (x < bx && dx < 0.) || (x > tx && dx > 0.)  
+  let contact_y y dy = let (_, _, ty) = E.bords
+  in (y > ty && dy > 0.)
+
+  let rebond_x x dx = if contact_x x dx then
+    -. dx
+  else dx
+
+  let rebond_y y dy = if contact_y y dy then
+    -. dy
+  else dy
+
     let rec run : position -> int -> position
     = fun ((px, py), (vx, vy)) r ->
       let acceleration =  (0.,g)
-      in let speed = (integre E.dt (vx, vy) acceleration)
-      in let position = (integre E.dt (px, py) speed)
-      in (position, speed)
+      in let (dvx, dvy) = (integre E.dt (vx, vy) acceleration)
+      in let (dpx, dpy) = (integre E.dt (px, py) (dvx, dvy))
+      in if contact_x dpx dvx then ((dpx, dpy), (rebond_x dpx dvx, dvy))
+      else if contact_y dpy dvy then ((dpx, dpy), (dvx, rebond_y dpy dvy))
+      else ((dpx, dpy), (dvx, dvy))
 end
